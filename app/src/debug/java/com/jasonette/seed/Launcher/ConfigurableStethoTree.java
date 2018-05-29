7f145ed743a92773372c40ab942bbadc3635b717
Swagata Acharyya <swagata@simpragma.com>
2018-05-29 12:45:58 +0530

    First demo release

--- app/src/debug/java/com/jasonette/seed/Launcher/ConfigurableStethoTree.java
+++ app/src/debug/java/com/jasonette/seed/Launcher/ConfigurableStethoTree.java
@@ -8,9 +8,11 @@ package com.jasonette.seed.Launcher;
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 import android.util.Log;
-import com.facebook.stetho.inspector.console.CLog;
-import com.facebook.stetho.inspector.console.ConsolePeerManager;
-import com.facebook.stetho.inspector.protocol.module.Console;
+//import com.facebook.stetho.inspector.console.CLog;
+//import com.facebook.stetho.inspector.console.ConsolePeerManager;
+//import com.facebook.stetho.inspector.protocol.module.Console;
+import java.io.Console;
+
 import timber.log.Timber;
 
 /**
@@ -48,53 +50,53 @@ public class ConfigurableStethoTree extends Timber.DebugTree {
     @Override
     protected void log(int priority, String tag, String message, Throwable t) {
 
-        if(priority < mConfiguration.mMinimumPriority) {
-            return;
-        }
-
-        ConsolePeerManager peerManager = ConsolePeerManager.getInstanceOrNull();
-        if (peerManager == null) {
-            Log.println(priority, tag, message);
-            return;
-        }
-
-        Console.MessageLevel logLevel;
-
-        switch (priority) {
-            case Log.VERBOSE:
-            case Log.DEBUG:
-                logLevel = Console.MessageLevel.DEBUG;
-                break;
-            case Log.INFO:
-                logLevel = Console.MessageLevel.LOG;
-                break;
-            case Log.WARN:
-                logLevel = Console.MessageLevel.WARNING;
-                break;
-            case Log.ERROR:
-            case Log.ASSERT:
-                logLevel = Console.MessageLevel.ERROR;
-                break;
-            default:
-                logLevel = Console.MessageLevel.LOG;
-        }
-
-        StringBuilder messageBuilder = new StringBuilder();
-
-        if(mConfiguration.mShowTags && tag != null) {
-            messageBuilder
-                .append("[")
-                .append(tag)
-                .append("] ");
-        }
-
-        messageBuilder.append(message);
-
-        CLog.writeToConsole(
-            logLevel,
-            Console.MessageSource.OTHER,
-            messageBuilder.toString()
-        );
+//        if(priority < mConfiguration.mMinimumPriority) {
+//            return;
+//        }
+//
+//        ConsolePeerManager peerManager = ConsolePeerManager.getInstanceOrNull();
+//        if (peerManager == null) {
+//            Log.println(priority, tag, message);
+//            return;
+//        }
+//
+//        Console.MessageLevel logLevel;
+//
+//        switch (priority) {
+//            case Log.VERBOSE:
+//            case Log.DEBUG:
+//                logLevel = Console.MessageLevel.DEBUG;
+//                break;
+//            case Log.INFO:
+//                logLevel = Console.MessageLevel.LOG;
+//                break;
+//            case Log.WARN:
+//                logLevel = Console.MessageLevel.WARNING;
+//                break;
+//            case Log.ERROR:
+//            case Log.ASSERT:
+//                logLevel = Console.MessageLevel.ERROR;
+//                break;
+//            default:
+//                logLevel = Console.MessageLevel.LOG;
+//        }
+//
+//        StringBuilder messageBuilder = new StringBuilder();
+//
+//        if(mConfiguration.mShowTags && tag != null) {
+//            messageBuilder
+//                .append("[")
+//                .append(tag)
+//                .append("] ");
+//        }
+//
+//        messageBuilder.append(message);
+//
+//        CLog.writeToConsole(
+//            logLevel,
+//            Console.MessageSource.OTHER,
+//            messageBuilder.toString()
+//        );
 
     }
 
